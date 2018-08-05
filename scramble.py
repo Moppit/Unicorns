a80b6c209fc7e48799799e40a08a265b3d9fcfeb
Michelle Tran <michelletranusa@gmail.com>
2018-08-04 23:45:58 -0600

    Finished! All bugs fixed

--- scramble.py
+++ scramble.py
@@ -1,12 +1,28 @@
 import random
 
 def scramble(string):
-    l = len(string)
-    for i in range(len(string)/2):
-        n = string[i+random.randint(0,5)]
-        string = string.translate(None, n)
-        for i in range(l-len(string)):
-            string += n
-    return string
+    pt1 = ""
+    for i in range(4):
+        pt1 += string[i+3] + string[i+8]
+    pt1 += string[7]
+    
+    pt2 = ""
+    start = 23
+    for j in range(9):
+        pt2 += string[start-j]
         
-print scramble("https://tinyurl.com/2fcpre6")
+    pt3 = string[:3] + string[12:15] + string[24:27]
+    
+    string_new = pt1 + pt2 + pt3
+
+    l = len(string_new)
+    for i in range(len(string_new)/2):
+        n = string_new[i+random.randint(0,len(string_new)/3)]
+        string_new = string_new.translate(None, n)
+        for i in range(l-len(string_new)):
+            string_new += n
+    return string_new
+    
+scrambled_message = "m/2hts/ttp:/fcpoc.lruynire6"
+        
+print scramble(scrambled_message)
